5953643625b91204ef06cc383303013f180218d3
unknown <clerollep@ucentral.edu.co>
2024-01-01 19:25:05 -0500

    back en nest js

---
+++ src/tasks/dto/task.dto.ts
@@ -0,0 +1,12 @@
+import { TaskStatus } from "../tasks.entity"
+
+export class CreateTaskDto {
+    title: string;
+    description: string;
+}
+
+export class UpdateTaskDto{
+    title?: string;
+    description?: string;
+    status?: TaskStatus;
+}
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto, UpdateTaskDto } from './dto/task.dto';
+
+@Controller('tasks')
+export class TasksController {
+
+    constructor(private taskService: TasksService) { }
+
+    @Get()
+    getAllTasks() {
+        return this.taskService.getAllTasks();
+    }
+
+    @Post()
+    createTask(@Body() newTask: CreateTaskDto) {
+        return this.taskService.createTask(newTask.title, newTask.description);
+    }
+
+    @Delete(':id')
+    deleteTask(@Param('id') id: string) {
+        this.taskService.deleteTask(id);
+    }
+    
+    @Patch(':id')
+    updateTask(@Param('id') id: string, @Body() updatedFields: UpdateTaskDto){
+        this.taskService.updateTask(id, updatedFields)
+    }
+}
